a03b1d1c6135754d00ee0a08e9c41a2a735a1e33
Stephen <sfigart@Macintosh.local>
2016-01-31 08:29:00 -1000

    Updated to use sendgrid for production

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,5 +1,5 @@
 class RegistrationMailer < ActionMailer::Base
-  default from: "palmoanalua@gmail.com"
+  default from: "palmacbaseball@gmail.com"
   
   def registration_complete(registration)
     @registration = registration
@@ -8,6 +8,8 @@ class RegistrationMailer < ActionMailer::Base
   
   def baseball_agreement(registration)
     @registration = registration
-    mail(:to => registration.email_address, :subject => "MAC Baseball Agreement")
+    mail(:to => registration.email_address,
+    	 :subject => "MAC Baseball Agreement",
+    	 :cc => "palmacbaseball@gmail.com")
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,13 @@ PalMoanalua::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: "macsports.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
   }
 
   # Log the query plan for queries taking more than this (works
